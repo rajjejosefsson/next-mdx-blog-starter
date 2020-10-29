c63ddcedf21ae44c4d0c1e43d34c250e72969655
Rasmus Josefsson <rajosefs@microsoft.com>
2020-10-29 12:03:03 +0100

    test seo component

--- pages/_app.js
+++ pages/_app.js
@@ -1,3 +1,22 @@
+import {DefaultSeo} from 'next-seo'
+
 export default function App({Component, pageProps}) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <DefaultSeo
+        openGraph={{
+          type: 'website',
+          locale: 'en_IE',
+          url: 'https://www.url.ie/',
+          site_name: 'SiteName',
+        }}
+        twitter={{
+          handle: '@handle',
+          site: '@site',
+          cardType: 'summary_large_image',
+        }}
+      />
+      <Component {...pageProps} />
+    </>
+  )
 }
