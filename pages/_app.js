05c974e2ecbc5951d8b1e6bb0191ec05a68ad84d
Rasmus Josefsson <rajosefs@microsoft.com>
2020-10-29 12:13:40 +0100

    test seo component

--- pages/_app.js
+++ pages/_app.js
@@ -1,23 +1,11 @@
 import {DefaultSeo} from 'next-seo'
 
+import config from '../config/seo'
+
 export default function App({Component, pageProps}) {
   return (
     <>
-      <DefaultSeo
-        title="site title"
-        description="site description"
-        openGraph={{
-          type: 'website',
-          locale: 'en_IE',
-          url: 'https://www.url.ie/',
-          site_name: 'SiteName',
-        }}
-        twitter={{
-          handle: '@handle',
-          site: '@site',
-          cardType: 'summary_large_image',
-        }}
-      />
+      <DefaultSeo {...config} />
       <Component {...pageProps} />
     </>
   )
